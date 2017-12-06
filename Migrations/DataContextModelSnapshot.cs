1576c580165f283f2374f56d1813e377cf0e9804
Alexandr SHad <shadurskij@gmail.com>
2017-12-06 17:32:12 +0200

    added ServiceType, PropertyType, PlaceId for Flat

--- Migrations/DataContextModelSnapshot.cs
+++ Migrations/DataContextModelSnapshot.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using RentApp.Models;
+using RentApp.Models.DbModels;
 using RentApp.Models.Structs;
 using System;
 
@@ -38,6 +39,11 @@ namespace RentApp.Migrations
 
                     b.Property<DateTime>("LastRepairDate");
 
+                    b.Property<string>("PlaceId")
+                        .IsRequired();
+
+                    b.Property<int>("PropertyType");
+
                     b.Property<DateTime>("ReleaseDate");
 
                     b.Property<int>("RoomsCount");
@@ -47,6 +53,22 @@ namespace RentApp.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Flats");
+                });
+
+            modelBuilder.Entity("RentApp.Models.DbModels.FlatServiceType", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<Guid>("FlatId");
+
+                    b.Property<int>("ServiceType");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FlatId");
+
+                    b.ToTable("FlatServiceTypes");
                 });
 
             modelBuilder.Entity("RentApp.Models.DbModels.Message", b =>
@@ -97,6 +119,14 @@ namespace RentApp.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("RentApp.Models.DbModels.FlatServiceType", b =>
+                {
+                    b.HasOne("RentApp.Models.DbModels.Flat", "Flat")
+                        .WithMany()
+                        .HasForeignKey("FlatId")
+                        .OnDelete(DeleteBehavior.Cascade);
                 });
 #pragma warning restore 612, 618
         }
--- Models/DbModels/Flat.cs
+++ Models/DbModels/Flat.cs
@@ -9,6 +9,9 @@ namespace RentApp.Models.DbModels
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Description { get; set; }
+        public PropertyTypes PropertyType { get; set; }
+        [Required]
+        public string PlaceId { get; set; }
         public float Cost { get; set; }
         public float Area { get; set; }
         public int RoomsCount { get; set; }
