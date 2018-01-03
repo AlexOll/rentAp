4d8d0db2b12dddd98ce135c338e57c28b6d5f73e
Andrii Shcherba <ashcherba.work@gmail.com>
2018-01-03 22:21:06 +0200

    added RealEstate db models

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -14,6 +14,14 @@ namespace RentApp.Models
         public DbSet<Flat> Flats { get; set; }
 	    public DbSet<FlatServiceType> FlatServiceTypes { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<RealEstateObject> RealEstateObjects { get; set; }
+        public DbSet<BaseRealEstateDetailes> RealEstateDetailes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GarageDetailes>().ToTable("GarageDetailes");
+            modelBuilder.Entity<AccommodationDetailes>().ToTable("AccommodationDetailes");
+        }
 
     }
 }
